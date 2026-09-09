75746bc8cfa3a6dd0f357978e5c88c5075716fb3
AlissonFA <alissonfreitas031@gmail.com>
2026-09-09 09:04:37 -0300

    Melhorar formatação

--- claude/8-arrays/script.js
+++ claude/8-arrays/script.js
@@ -1,33 +1,42 @@
 // 1
-const frutas = new Array("Maça", "Banana", "Uva", "Melancia", "Morango", "Abacate", "Laranja");
+const frutas = new Array(
+  "Maçã",
+  "Banana",
+  "Uva",
+  "Melancia",
+  "Morango",
+  "Abacate",
+  "Laranja",
+);
 const nomes = ["Alisson", "Alane", "Lucas"];
 
-console.log(frutas)
-console.log(nomes)
+console.log(frutas);
+console.log(nomes);
 
 // 2
-console.log(frutas[2])
+console.log(frutas[2]);
 
 // 3
-const frase = "No campo da tecnologia, a simplificação é sempre um grande avanço."
-console.log(frase.split(" "))
+const frase =
+  "No campo da tecnologia, a simplificação é sempre um grande avanço.";
+console.log(frase.split(" "));
 
 // 4
-frutas.push("Melão")
-console.log(frutas)
+frutas.push("Melão"); // Adiciona ao final
+console.log(frutas);
 
-frutas.pop()
-console.log(frutas)
+frutas.pop(); // Remove do final
+console.log(frutas);
 
-nomes.unshift("Alan")
-console.log(nomes)
+nomes.unshift("Alan"); // Adiciona ao início
+console.log(nomes);
 
-nomes.shift()
-console.log(nomes)
+nomes.shift(); // Remove do início
+console.log(nomes);
 
 // 5
-frutas[1] = "Melão"
-console.log(frutas)
+frutas[1] = "Melão";
+console.log(frutas);
 
 // 6
-console.log(frutas.includes("Uva"))
+console.log(frutas.includes("Uva"));
