a017455679b04bf4f0bf841fa62f8b8b8287e3ff
AlissonFA <alissonfreitas031@gmail.com>
2026-09-17 10:08:29 -0300

    Estudo prático de strict mode

---
+++ gemini/1-javascript-moderno/1-ecmascript-strict-mode/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+aura = "ola";
+
+function soma(a, a) {}
+
+let x = 10;
+
+console.log(x)
+
+// O strict mode é importante porque transforma erros silenciosos em falhas reais, proibindo práticas mal estruturadas, garantindo mais segurança.
